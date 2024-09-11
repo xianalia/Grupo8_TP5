48a2bb192ad6e360c0cf43cc0119616f97dd9ca4
sanchoponcho08 <kevinsito1559@gmail.com>
2024-09-11 20:14:27 -0300

    inicio probando si anda git

--- src/grupo8_tp5/Grupo8_TP5.java
+++ src/grupo8_tp5/Grupo8_TP5.java
@@ -1,21 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+
 package grupo8_tp5;
 
-/**
- *
- * @author xiana
- */
+
 public class Grupo8_TP5 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        Formulario formulario = new Formulario();
+        formulario.setVisible(true);
+        formulario.setLocationRelativeTo(null);
+        
+       Directorio directorio = new Directorio();
+      
+       
     }
     
 }
